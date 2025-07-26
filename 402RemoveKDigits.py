7df4cfa5da2c0b3767f2a7818ed772676faf3849
Shwetha K <shweetha.sweety17@gmail.com>
2025-07-26 10:47:30 +0530

    25-07-24 :: Shwetha K : Stack : Online Stock Span, Remove K Digits

--- 402RemoveKDigits.py
+++ 402RemoveKDigits.py
@@ -1,5 +1,6 @@
 '''
-Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+Given string num representing a non-negative integer num, and an integer k, 
+return the smallest possible integer after removing k digits from num.
 
  
 
